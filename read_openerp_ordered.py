a18c0b0b98c59df0d18a5efe76c7fdd86ee1a111
alan-guzman <aguzman@vauxoo.com>
2014-09-04 13:18:46 -0500

    Update read_openerp_ordered.py

--- read_openerp_ordered.py
+++ read_openerp_ordered.py
@@ -162,7 +162,7 @@ for line in content:
 
 print diccionario
 """
-
+"""
 
 import json
 import ast
@@ -240,3 +240,100 @@ odict_str = json.dumps(odict, indent=4)
 new_dict_str += odict_str
 #print "new_dict_str"
 print new_dict_str
+"""
+
+import json
+import os
+import sys
+import ast
+import collections
+from collections import OrderedDict
+import glob
+
+#~ my_path = "/home/jage/base_user_signature_logo/__openerp__.py"
+my_path="/home/jage/alan-7.0-addons-vauxoo/7.0/account_advance_payment"
+
+class DictToOrdered(ast.NodeTransformer):
+    def visit_Dict(self, node):
+        return ast.fix_missing_locations(ast.copy_location(
+            ast.Call(
+                func=ast.Attribute(
+                    value=ast.Name(id='collections', ctx=ast.Load()),
+                    attr='OrderedDict',
+                    ctx=ast.Load()),
+                args=[ast.Tuple(elts=
+                        [ast.Tuple(elts=list(pair), ctx=ast.Load())
+                         for pair in zip(node.keys, node.values)],
+                        ctx=ast.Load())],
+                keywords=[],
+                starargs=None,
+                kwargs=None),
+            node))
+
+def parse_dict_as_odict(src):
+    parsed = ast.parse(src, '<dynamic>', 'eval')
+    transformed = DictToOrdered().visit(parsed)
+    compiled = compile(transformed, '<dynamic>', 'eval')
+    print dir(eval(compiled)),'wwwwwwwwwww'
+    eval(compiled).popitem('description')
+    return eval(compiled)
+
+def dict_key_rename( dict, old_key, new_key ):
+    if dict.has_key( old_key):
+        if dict.has_key( new_key):
+            if isinstance( dict[new_key], list) and isinstance( dict[old_key], list):
+                dict[new_key].extend( dict.pop(old_key) )
+        else:
+            dict[new_key] = dict.pop(old_key) 
+    return dict
+ 
+#~ module_dir = sys.argv[1]
+#~ for dirpath, dnames, fnames in os.walk(module_dir):
+    #~ for f in fnames:
+        #~ if f.endswith("__openerp__.py"):
+            #~ fname = (os.path.join(dirpath, f))
+            #~ replace_all(fname, vale= new_dict_str)
+#~ aux = {'init_xml','data'}
+
+
+#~ modules_dir = my_path
+#~ for dirpath, dnames, fnames in os.walk(modules_dir):
+    #~ for f in fnames:
+        #~ if f.endswith("__openerp__.py"):
+            #~ fname = (os.path.join(dirpath, f))
+            #~ print fname
+            #~ replaceAll(fname, value = {
+                #~ 'openerp': ["osv", "tools.translate"],
+                #~ "openerp.addons": ["decimal_precision", "report_webkit"],
+                #~ "openerp.report":["report_sxw"],
+                #~ "rm":["import pooler"],
+
+
+modules_dir = my_path
+for dirpath, dnames, fnames in os.walk(modules_dir):
+    for f in fnames:
+        if f.endswith("__openerp__.py"):
+            fname = (os.path.join(dirpath, f))
+            print fname
+            
+            with open(fname) as fin:
+                 dict_str = fin.read()
+                 odict = parse_dict_as_odict( dict_str )
+                 #odict.rename('init_xml', 'data')
+                 print odict, "estamos probando lo que sea"
+                 odict = dict_key_rename(odict, 'init_xml', 'data')
+
+            new_dict_str = ""
+            for line in dict_str.splitlines():
+                print "imprimme line", line 
+                line = line.strip()
+                if line and line[0] == '#':
+                    #print line
+                    new_dict_str += line + '\n'
+            odict_str = json.dumps(odict, indent=4)
+            new_dict_str += odict_str
+            #print "new_dict_str"
+            print new_dict_str,"comentario"
+            #~ f2 = open(fname, 'w')
+            #~ f2.write(new_dict_str)
+            #~ f2.close()
