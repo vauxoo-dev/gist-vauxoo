f8717d16b125b4877dbd27815218a90f8a960478
luistorresm <luis_t@vauxoo.com>
2015-01-21 12:00:01 -0600

    Update bank_statement_ws.py
    Change name to put in move

--- bank_statement_ws.py
+++ bank_statement_ws.py
@@ -4,7 +4,7 @@ import time
 import csv
 import argparse
 import base64
-
+ 
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument("-d", "--db", help="DataBase Name", required=True)
 PARSER.add_argument("-r", "--user", help="OpenERP User", required=True)
@@ -18,30 +18,30 @@ PARSER.add_argument("-s", "--server",
                     help="Server IP, 127.0.0.1 for default",
                     default="127.0.0.1")
 ARGS = PARSER.parse_args()
-
+ 
 if ARGS.db is None or ARGS.user is None or ARGS.passwd is None:
     print "Must be specified DataBase, User and Password"
     quit()
-
+ 
 BANK_ST_LINE = ARGS.st
 DB_NAME = ARGS.db
 USER = ARGS.user
 PASSW = ARGS.passwd
 SERVER = ARGS.server
 PORT = ARGS.port
-
+ 
 OERP_CONNECT = oerplib.OERP(SERVER,
                             database=DB_NAME,
                             protocol='xmlrpc', port=PORT)
-
+ 
 UID_CONF = OERP_CONNECT.login(USER, PASSW)
-
+ 
 # ID de account bank statement line
 # Editar vista agregando campo "id" para obtener el valor
 st_line = OERP_CONNECT.browse('account.bank.statement.line', BANK_ST_LINE)
-
+ 
 move_bank_statement = []
-
+ 
 for move_line_id in st_line.journal_entry_id.line_id:
     if move_line_id.reconcile_id:
         for move_reconcile in move_line_id.reconcile_id.line_id:
@@ -55,8 +55,7 @@ for move_line_id in st_line.journal_entry_id.line_id:
                     'counterpart_move_line_id': move_reconcile.id,
                     'credit': move_reconcile.debit,
                     'debit': move_reconcile.credit,
-                    'name': move_reconcile.name})
-
+                    'name': move_reconcile.ref})
 # Al ejecutarse esta funcion desconcilia los movimientos que gurdamos en
 # @move_bank_statement tambien elimina la poliza
 # Se pone en un try la funcion por que hay un "bug" con oerplib cuando
@@ -68,7 +67,7 @@ try:
         [st_line.id])
 except:
     print "Error en return de metodo cancel"
-
+ 
 # Ejecuta el proceso de conciliacion de bank statement
 # Mismo "Bug" la funcion no tiene "Return" por eso esta en un try
 try:
