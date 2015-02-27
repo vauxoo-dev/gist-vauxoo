818bb05a652f986d568435bbee610920be931d5f
Luis Ernesto García Medina <ernesto_gm@vauxoo.com>
2015-02-27 00:03:17 -0600

    [REF][set_stock_move_acl.py] delete prints and change search of acc_move to search period is equals

--- set_stock_move_acl.py
+++ set_stock_move_acl.py
@@ -56,33 +56,22 @@ def change_aml(po, dbo, uo, pod, du, dp, dpo, dh):
             str(move.get('date')), '%Y-%m-%d %H:%M:%S').date().strftime(
             '%Y-%m-%d')
         account_period_obj = conect.get('account.period')
-        #import pdb;pdb.set_trace()
         period_date = account_period_obj.find(dt=date_move, context={})
         period_state = account_period_obj.browse(period_date[0]).state
-        print period_state
-        
         acc_m_ids = conect.search(
             'account.move', [
                 ('ref', '=', move.get('picking_id')[1]),
-                ('period_id', '!=', period_date[0])
+                ('period_id', '==', period_date[0])
             ])
         if period_state == "done" and acc_m_ids:
             account_period_obj.action_draft(period_date)
-            print account_period_obj.browse(period_date[0]).state
         for acc_mv in conect.browse('account.move', acc_m_ids):
             for line in acc_mv.line_id:
-                # cr = conp.cursor()
-                print move.get("id"), 'move.get("id")move.get("id")move.get("id")'
                 conect.write("account.move.line", line.id, {'sm_id': move.get("id")})
-                # cr.execute("""UPDATE account_move_line
-                #            SET sm_id='{move_id}'
-                #            WHERE id={amlid}""".format(
-                #      pid=period_date[0], move_id=move.get("id"), amlid=line.id))
                 file_new.write('%s, %s, %s\n' % (
                      str(line.id), line.name.encode('ascii', 'xmlcharrefreplace'), str(move.get("id")) or ''))
         if period_state == "done":
             wizard_period_close_id = conect.create('account.period.close', {'sure': 1})
-            print wizard_period_close_id, 'wizard_period_close_idwizard_period_close_idwizard_period_close_idwizard_period_close_id'
             conect.execute('account.period.close', 'data_save', [wizard_period_close_id], {
                 'lang': 'en_US',
                 'active_model': 'account.period',
@@ -90,9 +79,7 @@ def change_aml(po, dbo, uo, pod, du, dp, dpo, dh):
                 'tz': False,
                 'active_id': period_date
             })
-            print account_period_obj.browse(period_date[0]).state
     file_new.close()
-    conp.commit()
 
 if __name__ == '__main__':
     change_aml()
