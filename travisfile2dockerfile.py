3996454387655941e1c0d264bed294076bc743a8
moylop260 <moylop260@vauxoo.com>
2015-05-12 01:09:19 -0500

    [ADD] travis2docker: Add script to transform travis file to docker cmd script

---
+++ travisfile2dockerfile.py
@@ -0,0 +1,82 @@
+import itertools
+import re
+import types
+import yaml
+
+
+class travis(object):
+
+    def __init__(self, fname_travis_yml, fname_dockerfile):
+        f_travis_yml = open(fname_travis_yml, "r")
+        self.travis_data = yaml.load(f_travis_yml)
+        self.travis2docker_section = [
+            ('env', 'env'),
+            ('install', 'run'),
+            ('script', 'run'),
+        ]
+        self.travis2docker_section_dict = dict(self.travis2docker_section)
+        self.fname_dockerfile = fname_dockerfile
+        env_regex_str = r"(?P<var>[\w]*)[ ]*[\=][ ]*[\"\']{0,1}" + \
+            r"(?P<value>[\w\.\-\_/\$\{\}\:]*)[\"\']{0,1}"
+        export_regex_str = r"(?P<export>export|EXPORT)( )+" + env_regex_str
+        self.env_regex = re.compile(env_regex_str, re.M)
+        self.export_regex = re.compile(export_regex_str, re.M)
+        self.extra_env_from_run = ""
+
+    def get_travis_section(self, section):
+        section_type = self.travis2docker_section_dict.get(section, False)
+        if not section_type:
+            return None
+        section_data = self.travis_data[section]
+        if isinstance(section_data, basestring):
+            section_data = [section_data]
+        job_method = getattr(self, 'get_travis2docker_' + section_type)
+        return job_method(section_data)
+
+    def get_travis2docker_run(self, section_data):
+        docker_run = ''
+        for line in section_data:
+            for dummy, dummy, var, value in self.export_regex.findall(line):
+                self.extra_env_from_run += "\nexport %s=%s" % (var, value)
+            docker_run += '\n' + line
+        return docker_run
+
+    def get_travis2docker_env(self, section_data):
+        for line in section_data:
+            docker_env = ""
+            for var, value in self.env_regex.findall(line):
+                docker_env += "\nexport %s=%s" % (var, value)
+            yield docker_env
+
+    def get_travis2docker(self):
+        travis2docker_cmd_static_str = ""
+        travis2docker_cmd_iter_list = []
+        for travis_section, dummy in self.travis2docker_section:
+            travis2docker_section = self.get_travis_section(travis_section)
+            if isinstance(travis2docker_section, types.GeneratorType):
+                travis2docker_cmd_iter_list.append([
+                    item_iter for item_iter in travis2docker_section
+                ])
+            elif isinstance(travis2docker_section, basestring):
+                travis2docker_cmd_static_str += travis2docker_section + "\n"
+        count = 1
+        for item in itertools.product(*travis2docker_cmd_iter_list):
+            with open(
+                self.fname_dockerfile + str(count) + ".sh", "w"
+            ) as fdockerfile:
+                fdockerfile.write(
+                    item[0] + "\n" +
+                    # extra environment variables if you split section
+                    #   in many files cmd
+                    # self.extra_env_from_run + "\n" +
+                    travis2docker_cmd_static_str
+                )
+            count += 1
+
+
+if __name__ == '__main__':
+    FNAME_TRAVIS_YML2 = "/Users/moylop260/openerp/instancias/" + \
+        "odoo_git_clone/community-addons/yoytec/.travis.yml"
+    FNAME_DOCKERFILE2 = "/tmp/borrar/cmd.sh"
+    TRAVIS_OBJ = travis(FNAME_TRAVIS_YML2, FNAME_DOCKERFILE2)
+    TRAVIS_OBJ.get_travis2docker()
