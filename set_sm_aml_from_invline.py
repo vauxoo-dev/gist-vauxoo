0ad58ba3dcb71774f2d200fa174c281f81b61c28
Luis Ernesto García Medina <ernesto_gm@vauxoo.com>
2015-06-09 10:11:39 -0500

    [REF][set_sm_aml_from_invline.py] apply filter by date range, and write the acml records

--- set_sm_aml_from_invline.py
+++ set_sm_aml_from_invline.py
@@ -14,12 +14,12 @@ _logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.option('-po', default='admin', prompt='Password of Odoo',
+@click.option('-po', default='4DM1NCHUCK', prompt='Password of Odoo',
               help='Password of user Odoo')
-@click.option('-dbo', default='test', prompt='Database Odoo', help='DB Name')
-@click.option('-uo', default='admin', prompt='User Odoo', help='User of odoo')
+@click.option('-dbo', default='lodi_mayo', prompt='Database Odoo', help='DB Name')
+@click.option('-uo', default='nhomar@vauxoo.com', prompt='User Odoo', help='User of odoo')
 @click.option('-pod', default=8069, prompt='Port Odoo', help='Port of Odoo')
-@click.option('-du', default='admin', prompt='Database User',
+@click.option('-du', default='odoo', prompt='Database User',
               help='Name of database user')
 @click.option('-dp', default='1234', prompt='Database Password',
               help='Password of database user')
@@ -27,7 +27,11 @@ _logger = logging.getLogger(__name__)
               help='Port of Postgres')
 @click.option('-dh', default='localhost', prompt='Database Host',
               help='Host of Postgres')
-def change_aml(po, dbo, uo, pod, du, dp, dpo, dh):
+@click.option('-fini', prompt='Fecha de Inicio',
+              help='Fecha de Inicio')
+@click.option('-ffin', prompt='Fecha de Fin',
+              help='Fecha de Fin')
+def change_aml(po, dbo, uo, pod, du, dp, dpo, dh, fini, ffin):
     conect = oerplib.OERP('localhost', port=pod)
     conect.login(user=uo, passwd=po, database=dbo)
     conp = psycopg2.connect("dbname='{dn}' user='{du}' host='{dh}' "
@@ -37,16 +41,40 @@ def change_aml(po, dbo, uo, pod, du, dp, dpo, dh):
                                                                 dp=dp,
                                                                 dpo=dpo))
 
-    invoice_line = conect.search('account.invoice.line', [('move_id', '<>', False)])
+    dict_update = {}
+    invoice_ids = conect.search('account.invoice', [
+        ('date_invoice', '>=', fini),
+        ('date_invoice', '<=', ffin)])
+    invoice_line = conect.search(
+        'account.invoice.line',
+        [('invoice_id', 'in', invoice_ids), ('move_id', '<>', False)])
     for inv_line in conect.browse('account.invoice.line', invoice_line):
         move_line = conect.search('account.move.line', [
-                                            ('product_id', '=', inv_line.product_id.id),
-                                            ('move_id', '=', inv_line.invoice_id.move_id.id),
-                                            ])
-        print inv_line.invoice_id.id, inv_line.id, '_', inv_line.price_subtotal, inv_line.move_id
-        for line in conect.browse('account.move.line',move_line):
-            if inv_line.price_subtotal == line.credit or inv_line.price_subtotal == line.debit or inv_line.price_subtotal == line.amount_currency:
-                print line.id, '----', line.amount_currency, line.credit, line.debit
-
+            ('product_id', '=', inv_line.product_id.id),
+            ('move_id', '=', inv_line.invoice_id.move_id.id),
+            ('quantity', '=', inv_line.quantity)
+            ])
+        list_browse_acc_move_lines = conect.read(
+            'account.move.line', move_line,
+            ['credit', 'debit', 'amount_currency'])
+        sum_cred = sum(
+            [line.get('credit') for line in list_browse_acc_move_lines])
+        sum_deb = sum(
+            [line.get('debit') for line in list_browse_acc_move_lines])
+        sum_amount = sum(
+            [line.get(
+                'amount_currency') for line in list_browse_acc_move_lines])
+        for line in list_browse_acc_move_lines:
+            if inv_line.price_subtotal in line.values():
+                conect.write(
+                    'account.move.line', line.get('id'),
+                    {'sm_id': inv_line.move_id.id})
+                dict_update[line.get('id')] = inv_line.move_id.id
+            elif inv_line.price_subtotal in [sum_cred, sum_deb, sum_amount]:
+                conect.write(
+                    'account.move.line', line.get('id'),
+                    {'sm_id': inv_line.move_id.id})
+                dict_update[line.get('id')] = inv_line.move_id.id
+    print dict_update
 if __name__ == '__main__':
     change_aml()
