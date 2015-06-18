6ecc644744bec72253fb2cc185d583845389584a
Julio [Vauxoo] <julio@vauxoo.com>
2015-06-18 18:29:59 -0500

    Create update_message_id.sql
    This script update the message_id the part that contain the HostName when there is a change of server or HostName

---
+++ update_message_id.sql
@@ -0,0 +1,16 @@
+UPDATE mail_message
+SET message_id = new_message_id
+FROM (
+	SELECT id, 
+	   regexp_replace(message_id, '@.*>$', '@' || current_hostname || '>') AS new_message_id
+	FROM mail_message
+	INNER JOIN (
+		SELECT TEXT(
+			'vauxoo70.vauxoo.com'
+		) AS current_hostname
+	    ) var 
+	    ON 1=1
+	 WHERE message_id NOT LIKE '%@' || current_hostname || '>' 
+	   AND message_id <> 'True'
+) subvw
+WHERE mail_message.id = subvw.id
