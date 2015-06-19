33f8813400d6a3750e51b5c6f06622a6019025d7
Julio [Vauxoo] <julio@vauxoo.com>
2015-06-18 20:10:37 -0500

    Update update_message_id.sql
    Added example of message_id

--- update_message_id.sql
+++ update_message_id.sql
@@ -1,6 +1,13 @@
 -- When the part of hostname into message_id not correspond with the current hostname
 -- the received messages into Odoo are not associated with corresponding thread 
 -- With this script we can to update hostname into message_id to keep the expected behavior
+-- e.g.
+-- message_id with old hostname 'vauxoo70'
+-- <1415205837.104161977767944.602270230993340-openerp-2480-project.task@vauxoo70>
+-- message_id after to execute script with new hostname
+--<1415205837.104161977767944.602270230993340-openerp-2480-project.task@vauxoo70.vauxoo.com>
+
+
 UPDATE mail_message
 SET message_id = new_message_id
 FROM (
