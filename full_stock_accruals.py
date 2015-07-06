aafc9f7af4ae48049c6d4ebf95f0568e77508ca7
Humberto Arocha <hbto@vauxoo.com>
2015-07-06 03:02:47 -0500

    [IMP] Using memoize in code when searching and writing aml journal entry lines

--- full_stock_accruals.py
+++ full_stock_accruals.py
@@ -4,8 +4,10 @@ import oerplib
 import logging
 import datetime
 import click
+import time
 
 import psycopg2
+import functools
 _logger = logging.getLogger(__name__)
 #############################
 # ## Constants Declaration ###
@@ -30,25 +32,39 @@ _logger = logging.getLogger(__name__)
               help='Fecha de Inicio')
 @click.option('-ffin', prompt='Fecha de Fin (dd/mm/yyyy)',
               help='Fecha de Fin')
-def memoize(func):
-    """
-    Pro python Marty Alchin, Pag  75, Memoization
-    """
-    cache = {}
-
-    @functools.wraps(func)
-    def wrapper(*args):
-        if args in cache:
-            return cache[args]
-        result = func(*args)
-        cache[args] = result
-        return result
-    return wrapper
 
 
 def change_aml(po, dbo, uo, pod, du, dp, dpo, dh, fini, ffin):
+    time_start = time.ctime()
+    def memoize(func):
+        """
+        Pro python Marty Alchin, Pag  75, Memoization
+        """
+        cache = {}
+
+        @functools.wraps(func)
+        def wrapper(*args):
+            if args in cache:
+                # print "return cached value"
+                return cache[args]
+            result = func(*args)
+            cache[args] = result
+            # print "return value from DB"
+            return result
+        return wrapper
+
+    @memoize
+    def find_period(obj, date, context=None):
+        period_id = obj.find(dt=date, context=context)
+        return period_id and period_id[0] or False
+
+    @memoize
+    def browse_period_state(obj, ids, context=None):
+        return obj.browse(ids).state
+
     conect = oerplib.OERP('localhost', port=pod)
     conect.login(user=uo, passwd=po, database=dbo)
+    account_period_obj = conect.get('account.period')
     ffin = datetime.datetime.strptime(ffin, '%d/%m/%Y').strftime('%m/%d/%Y')
     fini = datetime.datetime.strptime(fini, '%d/%m/%Y').strftime('%m/%d/%Y')
     product_ids = conect.search(
@@ -61,40 +77,117 @@ def change_aml(po, dbo, uo, pod, du, dp, dpo, dh, fini, ffin):
             ('date', '>=', fini),
             ('date', '<=', ffin)])
     pick_ids = []
-    file_new = open('/tmp/moves_changeds.csv', 'wb')
-    file_new.write('id, name, date, period\n')
-    for move in conect.read('stock.move', move_ids, ['id', 'date', 'picking_id']):
+
+    # file_new = open('/tmp/moves_changeds.csv', 'wb')
+    # file_new.write('id, name, date, period\n')
+
+    # Open a new period variable so that:
+    # it can be remember the last state of the period and the revert it back to
+    # its previous state
+    period_restore = {}
+    reads = conect.read('stock.move', move_ids, ['id', 'date', 'picking_id'])
+    total = len(reads)
+    move_count = 0
+    for move in reads:
+
+        move_count += 1
+        print "[{count} / {total}]".format(total=total, count=move_count)
+
         if move.get('picking_id')[0] in pick_ids:
+            print "Picking already checked"
             continue
         else:
             pick_ids.append(move.get('picking_id')[0])
         date_move = datetime.datetime.strptime(
             str(move.get('date')), '%Y-%m-%d %H:%M:%S').date().strftime(
             '%Y-%m-%d')
-        account_period_obj = conect.get('account.period')
-        period_date = account_period_obj.find(dt=date_move, context={})
-        period_state = account_period_obj.browse(period_date[0]).state
+        # save this period this way we do not waste time searching several
+        # times for the same resource
+        # by using memoize technique
+        period_id = find_period(account_period_obj, date_move)
+        if not period_id:
+            continue
+
+        period_state = browse_period_state(account_period_obj, period_id)
+
         acc_m_ids = conect.search(
             'account.move', [
                 ('ref', '=', move.get('picking_id')[1]),
-                ('period_id', '=', period_date[0])
+                ('period_id', '=', period_id)
             ])
-        if period_state == "done" and acc_m_ids:
-            account_period_obj.action_draft(period_date)
-        for acc_mv in conect.browse('account.move', acc_m_ids):
-            conect.write("account.move.line", acc_mv.line_id.ids, {'sm_id': move.get("id")})
-            file_new.write('%s, %s\n' % (
-                 str(acc_mv.line_id.ids), str(move.get("id")) or ''))
-        if period_state == "done":
-            wizard_period_close_id = conect.create('account.period.close', {'sure': 1})
-            conect.execute('account.period.close', 'data_save', [wizard_period_close_id], {
+
+        if not acc_m_ids:
+            # We continue with next move, there nothing to do here
+            print "No Journal Entries"
+            continue
+
+        # we can save a lot of time by only opening once this record and after
+        # finishing it we set it back to its previous state
+        if period_state == "done" and not period_restore.get(period_id):
+            account_period_obj.action_draft([period_id])
+            period_restore[period_id] = period_state
+
+        count_am = 0
+        am_brws = conect.browse('account.move', acc_m_ids)
+        total_am = len(am_brws)
+        aml_ids = []
+        for acc_mv in am_brws:
+            count_am += 1
+            print "[{count} / {total}] [{count_am} / {total_am}] Reading Journal Entry {name}".format(
+                    count=move_count,
+                    total=total,
+                    count_am=count_am,
+                    total_am=total_am,
+                    name=acc_mv.name,
+                    )
+            # TODO: Optimize this is being done on all lines regardless
+            # reconcilable
+
+            aml_ids += acc_mv.line_id.ids
+
+            # aml_ids += [aml_brw.id for aml_brw in acc_mv.line_id
+            #            if aml_brw.account_id.reconcile]
+        if not aml_ids:
+            print "No Journal Items to update"
+            continue
+        conect.write(
+            "account.move.line", aml_ids,
+            {'sm_id': move.get("id")})
+        print "Finished writing on Journal Entry Lines"
+
+        # file_new.write(
+        #     '%s, %s\n' % (
+        #         str(aml_ids), str(move.get("id")) or ''))
+
+    # This process is to be done out the for loop to avoid wasting resource
+    # opening and closing same period several time
+    if period_restore:
+        print "Restoring Period States"
+    else:
+        print "No Periods to Restore"
+
+    for pid in period_restore.keys():
+        print "State id: {pid} restored".format(pid=pid)
+        wizard_period_close_id = conect.create(
+            'account.period.close', {'sure': 1})
+        conect.execute(
+            'account.period.close', 'data_save',
+            [wizard_period_close_id], {
                 'lang': 'en_US',
                 'active_model': 'account.period',
-                'active_ids': period_date,
+                'active_ids': [pid],
                 'tz': False,
-                'active_id': period_date
-            })
-    file_new.close()
+                'active_id': [pid]
+            }
+        )
+
+    # file_new.close()
+
+    time_stop = time.ctime()
+    print time_start
+    print time_stop
+
+    return True
 
     invoice_ids = conect.search('account.invoice', [
         ('date_invoice', '>=', fini),
