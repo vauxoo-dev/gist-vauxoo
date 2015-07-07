b5fde4f301f7a0133af584768b4d0a4b50ea0f1c
Luis Ernesto García Medina <ernesto_gm@vauxoo.com>
2015-07-07 13:28:10 -0500

    [REF][full_stock_accruals] Web services are added: set_sm_aml_from_invline and set_stock_move_acl in a single web service to have the entire process for stock_accruals

--- full_stock_accruals.py
+++ full_stock_accruals.py
@@ -4,7 +4,6 @@ import oerplib
 import logging
 import datetime
 import click
-import csv
 
 import psycopg2
 _logger = logging.getLogger(__name__)
@@ -41,6 +40,51 @@ def change_aml(po, dbo, uo, pod, du, dp, dpo, dh, fini, ffin):
                                                                 dp=dp,
                                                                 dpo=dpo))
 
+    product_ids = conect.search(
+        'product.product', [('valuation', '=', 'real_time')])
+    move_ids = conect.search(
+        'stock.move', [
+            ('product_id', 'in', product_ids),
+            ('picking_id', '!=', False),
+            ('state', '=', 'done'),
+            ('date', '>=', fini),
+            ('date', '<=', ffin)])
+    pick_ids = []
+    file_new = open('/tmp/moves_changeds.csv', 'wb')
+    file_new.write('id, name, date, period\n')
+    for move in conect.read('stock.move', move_ids, ['id', 'date', 'picking_id']):
+        if move.get('picking_id')[0] in pick_ids:
+            continue
+        else:
+            pick_ids.append(move.get('picking_id')[0])
+        date_move = datetime.datetime.strptime(
+            str(move.get('date')), '%Y-%m-%d %H:%M:%S').date().strftime(
+            '%Y-%m-%d')
+        account_period_obj = conect.get('account.period')
+        period_date = account_period_obj.find(dt=date_move, context={})
+        period_state = account_period_obj.browse(period_date[0]).state
+        acc_m_ids = conect.search(
+            'account.move', [
+                ('ref', '=', move.get('picking_id')[1]),
+                ('period_id', '=', period_date[0])
+            ])
+        if period_state == "done" and acc_m_ids:
+            account_period_obj.action_draft(period_date)
+        for acc_mv in conect.browse('account.move', acc_m_ids):
+            conect.write("account.move.line", acc_mv.line_id.ids, {'sm_id': move.get("id")})
+            file_new.write('%s, %s\n' % (
+                 str(acc_mv.line_id.ids), str(move.get("id")) or ''))
+        if period_state == "done":
+            wizard_period_close_id = conect.create('account.period.close', {'sure': 1})
+            conect.execute('account.period.close', 'data_save', [wizard_period_close_id], {
+                'lang': 'en_US',
+                'active_model': 'account.period',
+                'active_ids': period_date,
+                'tz': False,
+                'active_id': period_date
+            })
+    file_new.close()
+
     invoice_ids = conect.search('account.invoice', [
         ('date_invoice', '>=', fini),
         ('date_invoice', '<=', ffin)])
@@ -61,6 +105,8 @@ def change_aml(po, dbo, uo, pod, du, dp, dpo, dh, fini, ffin):
                 conect.write(
                     'account.move.line', line.id,
                     {'sm_id': inv_line.move_id.id})
-        
+
+    conect.get('account.invoice').reconcile_stock_accrual(invoice_ids)
+
 if __name__ == '__main__':
     change_aml()
