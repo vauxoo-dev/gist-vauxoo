2190f326d290488aa80da26f21caa7b4b4b28ec4
Luis Ernesto García Medina <ernesto_gm@vauxoo.com>
2015-10-24 09:36:16 -0500

    [ADD] added new webservice to receive the products of confirmed purchase order and send to correct location, and cancel stock picking from second move of input to Stock

---
+++ picking_wh_reestructure.py
@@ -0,0 +1,73 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import oerplib
+import logging
+import datetime
+import click
+import csv
+
+import psycopg2
+_logger = logging.getLogger(__name__)
+#############################
+# ## Constants Declaration ###
+#############################
+
+
+@click.command()
+@click.option('-po', default='4DM1NCHUCK', prompt='Password of Odoo',
+              help='Password of user Odoo')
+@click.option('-dbo', default='lodi_test_wh', prompt='Database Odoo', help='DB Name')
+@click.option('-uo', default='nhomar@vauxoo.com', prompt='User Odoo', help='User of odoo')
+@click.option('-pod', default=8069, prompt='Port Odoo', help='Port of Odoo')
+@click.option('-du', default='odoo', prompt='Database User',
+              help='Name of database user')
+@click.option('-dp', default='1234', prompt='Database Password',
+              help='Password of database user')
+@click.option('-dpo', default=5432, prompt='Database Port',
+              help='Port of Postgres')
+@click.option('-dh', default='localhost', prompt='Database Host',
+              help='Host of Postgres')
+
+def change_aml(po, dbo, uo, pod, du, dp, dpo, dh):
+    conect = oerplib.OERP('localhost', port=pod)
+    conect.login(user=uo, passwd=po, database=dbo)
+    conp = psycopg2.connect("dbname='{dn}' user='{du}' host='{dh}' "
+                            "password='{dp}' port={dpo}".format(dn=dbo,
+                                                                du=du,
+                                                                dh=dh,
+                                                                dp=dp,
+                                                                dpo=dpo))
+    picking_type_loc = {1: 1521, 19: 1562}  # se coloca la ubicacion de acuerdo al picking_type
+    for picking_type in picking_type_loc.keys():
+        purchase_no_confirmed = conect.search('purchase.order', [
+            ('state', 'in', ('draft', 'confirmed')),
+            ('picking_type_id', '=', picking_type)
+            ])
+        for po in conect.browse('purchase.order', purchase_no_confirmed):
+            conect.write('purchase.order', po.id, {'location_id': picking_type_loc[picking_type]})
+
+        purchase_ids = conect.search('purchase.order', [
+            ('state', '=', "approved"),
+            ('picking_type_id', '=', picking_type)
+            ])
+        for po in conect.browse('purchase.order', purchase_ids):
+            conect.write('purchase.order', po.id, {'location_id': picking_type_loc[picking_type]})
+            for picking_received in po.picking_ids:
+                if picking_received.state == 'assigned':
+                    wz_trans_id = conect.create(
+                        'stock.transfer_details', {'picking_id': picking_received.id})
+                    for line in picking_received.move_lines:
+                        conect.write('stock.move', line.id, {'location_dest_id': picking_type_loc[picking_type]})
+                        conect.create('stock.transfer_details_items', {
+                            'quantity': line.product_uom_qty,
+                            'sourceloc_id': line.location_id.id,
+                            'destinationloc_id': line.location_dest_id.id,
+                            'transfer_id': wz_trans_id,
+                            'product_id': line.product_id.id,
+                            'product_uom_id': line.product_id.uom_id.id})
+                    conect.get('stock.transfer_details').do_detailed_transfer(
+                        [wz_trans_id])
+            # Para cancelar el o los restantes de la  orden de compra Entrada->Existencias
+            conect.get('stock.picking').action_cancel(conect.search('stock.picking', [('origin', 'like', po.name), ('state', '!=', 'done')]))
+if __name__ == '__main__':
+    change_aml()
