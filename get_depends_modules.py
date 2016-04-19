e0aaf482f8c5a368d593638e0bf892dbb6b51442
Moisés López <moylop260@vauxoo.com>
2016-04-19 00:26:10 -0500

    [FIX] get_depends_modules:  odoo-8.0

--- get_depends_modules.py
+++ get_depends_modules.py
@@ -1,10 +1,10 @@
 import re
- 
-#logfile = "/Users/moylop260/Desktop/log1.log"
-logfile = "/Users/moylop260/Desktop/log1_error.log"
-re_module_log = "(: module )(?P<module>[\w,\-,\_]+):( loading )(?P<file>[\w,-,_/.]+)"
-module_to_search_previous_depends = 'sale_stock'
- 
+
+
+logfile = "/Users/moylop260/Downloads/job_20_test_all.txt"
+re_module_log = r"( openerp.modules.module: module )(?P<module>[\w,\-,\_]+):( creating or updating database tables)"
+module_to_search_previous_depends = 'mail_notification_picking'
+
 modules = []
 files = []
 with open(logfile, "r") as f:
@@ -12,11 +12,12 @@ with open(logfile, "r") as f:
         match_object = re.search( re_module_log, line )
         if match_object:
             module = match_object.group("module")
-            if not module in modules: 
+            if not module in modules:
                 modules.append( module )
-            files.append( module + '/' + match_object.group("file") )
- 
-if module_to_search_previous_depends in modules:
-    print ','.join( modules[ :modules.index('sale_stock') +1 ] )
+            # files.append( module + '/' + match_object.group("file") )
 
-print '\n'.join(files)
+if module_to_search_previous_depends in modules:
+    print ','.join(
+        modules[:modules.index(module_to_search_previous_depends) + 1] )
+
+# print '\n'.join(files)
