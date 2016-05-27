ef3c7af14ab05aa5ca5bf66271a59cd01787644e
Moisés López <moylop260@vauxoo.com>
2016-05-27 22:27:59 +0000

    [REF] cursores.py: Add isolated connection

--- cursores.py
+++ cursores.py
@@ -1,19 +1,42 @@
+#!/usr/bin/python
 import psycopg2
-conn = psycopg2.connect("dbname='omlLuis5'")
-conn2 = psycopg2.connect("dbname='omlLuis5'")
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT, ISOLATION_LEVEL_READ_COMMITTED, ISOLATION_LEVEL_REPEATABLE_READ
+
+
+dbname = 'openerp_test29'
+serialized = False  # True
+
+if serialized:
+    isolation_level = ISOLATION_LEVEL_REPEATABLE_READ
+else:
+    isolation_level = ISOLATION_LEVEL_READ_COMMITTED
+
+# connection 1
+conn = psycopg2.connect("dbname='%s'" % dbname)
+conn.set_isolation_level(isolation_level)
+import pdb;pdb.set_trace()
 cur = conn.cursor()
-print id(cur)
-cur.execute('SELECT id, name FROM account_journal;')
-print cur.fetchall()
 
+SELECT_QUERY = "SELECT id, name FROM account_journal WHERE id = %s"
+UPDATE_QUERY = "UPDATE account_journal SET name = 'Moy' WHERE id = %s"
+journal_id = 1
+
+cur.execute(SELECT_QUERY, (journal_id,))
+print "11", cur.fetchall()
+
+cur.execute(UPDATE_QUERY, (journal_id,))
+# conn.commit()
+cur.execute(SELECT_QUERY, (journal_id,))
+print "12", cur.fetchall()
+
+# connection 2
+conn2 = psycopg2.connect("dbname='%s'" % dbname)
+conn2.set_isolation_level(isolation_level)
 cur2 = conn2.cursor()
-print id(cur2)
 
-cur.execute("UPDATE account_journal SET name = 'Moy' WHERE id = 1;")
-print dir(cur)
-conn.commit()
-cur.execute('SELECT id, name FROM account_journal;')
-print cur.fetchall()
-
-cur2.execute('SELECT id, name FROM account_journal;')
-print cur2.fetchall()
+journal_id = 2
+print "29"
+cur2.execute(UPDATE_QUERY, (journal_id,))
+print "291"
+cur2.execute(SELECT_QUERY, (journal_id,))
+print "21", cur2.fetchall()
