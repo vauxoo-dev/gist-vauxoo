fd85a5ae6fba7b130f53cf422579927b7bf73dc1
Moisés López <moylop260@vauxoo.com>
2016-05-28 14:44:47 +0000

    [REF] cursores.py: Add case of use with ISOLATION_LEVEL_REPEATABLE_READ

--- cursores.py
+++ cursores.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python
+
 import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT, ISOLATION_LEVEL_READ_COMMITTED, ISOLATION_LEVEL_REPEATABLE_READ
+from psycopg2.extensions import ISOLATION_LEVEL_REPEATABLE_READ
 
 
-dbname = 'openerp_test29'
-serialized = False  # True
+# ISOLATION_LEVEL_REPEATABLE_READ:
+#   use cache to avoid read the database by each time
+def create_connection(dbname, isolation_level=ISOLATION_LEVEL_REPEATABLE_READ):
+    conn = psycopg2.connect("dbname='%s'" % dbname)
+    conn.set_isolation_level(isolation_level)
+    cur = conn.cursor()
+    return conn, cur
 
-if serialized:
-    isolation_level = ISOLATION_LEVEL_REPEATABLE_READ
-else:
-    isolation_level = ISOLATION_LEVEL_READ_COMMITTED
 
-# connection 1
-conn = psycopg2.connect("dbname='%s'" % dbname)
-conn.set_isolation_level(isolation_level)
-import pdb;pdb.set_trace()
-cur = conn.cursor()
-
+dbname = 'openerp_test30'
+conn1, cur1 = create_connection(dbname)
+conn2, cur2 = create_connection(dbname)
 SELECT_QUERY = "SELECT id, name FROM account_journal WHERE id = %s"
 UPDATE_QUERY = "UPDATE account_journal SET name = 'Moy' WHERE id = %s"
-journal_id = 1
+row_id = 1
 
-cur.execute(SELECT_QUERY, (journal_id,))
-print "11", cur.fetchall()
+cur1.execute(SELECT_QUERY, (row_id,))
+print "cursor 1 - select to add cache with ISOLATION_LEVEL_REPEATABLE_READ"
+print cur1.fetchall()
 
-cur.execute(UPDATE_QUERY, (journal_id,))
-# conn.commit()
-cur.execute(SELECT_QUERY, (journal_id,))
-print "12", cur.fetchall()
+cur2.execute(SELECT_QUERY, (row_id,))
+print "cursor 2 - select to add cache with ISOLATION_LEVEL_REPEATABLE_READ"
+print cur2.fetchall()
 
-# connection 2
-conn2 = psycopg2.connect("dbname='%s'" % dbname)
-conn2.set_isolation_level(isolation_level)
-cur2 = conn2.cursor()
+cur1.execute(UPDATE_QUERY, (row_id,))
+conn1.commit()
+print "cursor 1 - update row and commit"
 
-journal_id = 2
-print "29"
-cur2.execute(UPDATE_QUERY, (journal_id,))
-print "291"
-cur2.execute(SELECT_QUERY, (journal_id,))
-print "21", cur2.fetchall()
+cur1.execute(SELECT_QUERY, (row_id,))
+print "cursor 1 - read again (updated cache)", cur1.fetchall()
+
+cur2.execute(SELECT_QUERY, (row_id,))
+print "cursor 2 - read again (outdated cache)", cur2.fetchall()
