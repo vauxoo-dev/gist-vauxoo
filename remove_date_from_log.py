963073c3f575028598b3a5679788d2cecc37cc2d
Moisés López <moylop260@vauxoo.com>
2016-06-29 06:36:54 +0000

    [REF] remove_date_from_log: Add print modules list

--- remove_date_from_log.py
+++ remove_date_from_log.py
@@ -1,12 +1,20 @@
 import re
- 
-logfile = "/Users/moylop260/Desktop/log1_error.log"
+
+logfile = "job_20_test_all.txt"
 re_module_log = "(openerp)(?P<module>[\D|\d])+"
+re_module_name = re.compile(
+    r"(openerp\.modules\.module\: module )(?P<module>(\w|_)+):")
 
 newfile = open(logfile + '.out', "wb")
-with open(logfile, "r") as f:
-    for line in f.readlines():
-        match_object = re.search( re_module_log, line )
+modules = []
+with open(logfile, "r") as flog:
+    for line in flog:
+        match_object = re.search(re_module_log, line)
         if match_object:
             newfile.write(match_object.group(0))
+            match_module = re_module_name.findall(line)
+            if match_module:
+                modules.append(match_module[0][1])
     newfile.close()
+
+print ','.join(modules)
