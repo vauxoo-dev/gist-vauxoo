2e0c6283d65db54078300ceee0d3f0ce916764e2
goliveirab <germanaoliveirab@gmail.com>
2017-05-30 18:23:13 -0400

    [ADD] server action: Update partner journal item

--- update_partner_journal_item.py
+++ update_partner_journal_item.py
@@ -1,55 +1,19 @@
-# Fixed XML when is not decoded
-import oerplib
-import argparse
-from codecs import BOM_UTF8
-BOM_UTF8U = BOM_UTF8.decode('UTF-8')
-
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument("-d", "--db", help="DataBase Name", required=True)
-PARSER.add_argument("-r", "--user", help="OpenERP User", required=True)
-PARSER.add_argument("-w", "--passwd", help="OpenERP Password", required=True)
-PARSER.add_argument("-p", "--port",
-                    type=int,
-                    help="Port, 8069 for default", default="8069")
-PARSER.add_argument("-s", "--server",
-                    help="Server IP, 127.0.0.1 for default",
-                    default="127.0.0.1")
-ARGS = PARSER.parse_args()
-
-if ARGS.db is None or ARGS.user is None or ARGS.passwd is None:
-    print "Must be specified DataBase, User and Password"
-    quit()
-
-DB_NAME = ARGS.db
-USER = ARGS.user
-PASSW = ARGS.passwd
-SERVER = ARGS.server
-PORT = ARGS.port
-
-OERP_CONNECT = oerplib.OERP(SERVER,
-                            database=DB_NAME,
-                            protocol='xmlrpc', port=PORT)
-OERP_CONNECT.config['timeout'] = 600  # Set the timeout to 300 seconds
-
-UID_CONF = OERP_CONNECT.login(USER, PASSW)
-
-company_obj = OERP_CONNECT.get('res.company')
-aml_obj = OERP_CONNECT.get('account.move.line')
-move_obj = OERP_CONNECT.get('account.move')
-ait_obj = OERP_CONNECT.get('account.invoice.tax')
-invoice_obj = OERP_CONNECT.get('account.invoice')
-tax_obj = OERP_CONNECT.get('account.tax')
-
+# Server Acion
+# Base mode: Journal item
+company_obj = env['res.company']
+aml_obj = env['account.move.line']
+move_obj = env['account.move']
+ait_obj = env['account.invoice.tax']
+invoice_obj = env['account.invoice']
+tax_obj = env['account.tax']
 journal_ids = []
-for company in company_obj.browse(company_obj.search([])):
+for company in company_obj.search([]):
     if company.tax_cash_basis_journal_id:
         journal_ids.append(company.tax_cash_basis_journal_id.id)
-
 invoice_ids = invoice_obj.search([
     ('type', '=', 'in_invoice'),
     ('state', '=', 'paid'),
 ])
-
 fixeds = []
 move_dict = {
     24429: 197,
@@ -100,16 +64,17 @@ move_dict = {
     28620: 43,
     24372: 365,
     24594: 346,
+    33644: 334,
+    28254: 334,
 }
 for move in move_dict:
-    OERP_CONNECT.execute('account.move', 'button_cancel', [move])
+    move_obj.browse(move).button_cancel()
     lines = move_obj.browse(move).line_ids.ids
-    aml_obj.write(lines, {'partner_id': move_dict.get(move)})
+    aml_obj.browse(lines).write({'partner_id': move_dict.get(move)})
     fixeds.extend(lines)
-    OERP_CONNECT.execute('account.move', 'post', [move])
-
+    move_obj.browse(move).post()
 taxes = tax_obj.search([('type_tax_use', '=', 'purchase')])
-acc_tax = [t.cash_basis_account.id for t in tax_obj.browse(taxes) if t.cash_basis_account]
+acc_tax = [t.cash_basis_account.id for t in taxes if t.cash_basis_account]
 for journal in journal_ids:
     aml_ids = aml_obj.search([
         ('partner_id', '=', False),
@@ -117,26 +82,25 @@ for journal in journal_ids:
         ('tax_ids', '!=', False),
         ('ref', '=', False),
     ])
-    for line in aml_obj.browse(aml_ids):
+    for line in aml_ids:
         tax = tax_obj.browse(line.tax_ids.ids[0])
         if tax.type_tax_use != 'purchase' or line.id in fixeds:
             continue
         move = line.move_id
         invoice = invoice_obj.search(['|', ('amount_untaxed', '=', move.amount), ('amount_total', '=', move.amount), ('state', '=', 'paid')])
         if len(invoice) == 1:
-            OERP_CONNECT.execute('account.move', 'button_cancel', [move.id])
-            partner = invoice_obj.browse(invoice[0]).partner_id.id
-            aml_obj.write(move.line_ids.ids, {'partner_id': partner})
-            OERP_CONNECT.execute('account.move', 'post', [move.id])
+            move_obj.browse(move.id).button_cancel()
+            partner = invoice[0].partner_id.id
+            aml_obj.browse(move.line_ids.ids).write({'partner_id': partner})
+            move_obj.browse(move.id).post()
             fixeds.extend(move.line_ids.ids)
             continue
         elif invoice:
             partner = []
-            [partner.append(i.partner_id.id) for i in invoice_obj.browse(invoice) if i.partner_id.id not in partner]
+            [partner.append(i.partner_id.id) for i in invoice if i.partner_id.id not in partner]
             if len(partner) == 1:
-                OERP_CONNECT.execute('account.move', 'button_cancel', [move.id])
-                aml_obj.write(move.line_ids.ids, {'partner_id': partner[0]})
-                OERP_CONNECT.execute('account.move', 'post', [move.id])
+                move_obj.browse(move.id).button_cancel()
+                aml_obj.browse(move.line_ids.ids).write({'partner_id': partner[0]})
+                move_obj.browse(move.id).post()
                 fixeds.extend(move.line_ids.ids)
                 continue
-    
