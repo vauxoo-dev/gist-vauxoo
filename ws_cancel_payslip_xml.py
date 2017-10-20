f7674819bd61c5f8ce856ecf7d79b34b1301bda3
goliveirab <germana@vauxoo.com>
2017-10-20 18:52:39 +0000

    [REF]: refactoring cancel webservice, removing DB con, writing results to file, adding pac parameters

--- ws_cancel_payslip_xml.py
+++ ws_cancel_payslip_xml.py
@@ -3,7 +3,7 @@
 import argparse
 import base64
 import os
-import odoorpc
+import time
 import logging
 _logger = logging.getLogger(__name__)
 try:
@@ -13,69 +13,43 @@ except ImportError:
 
 
 PARSER = argparse.ArgumentParser(description="Cancel Payroll XML")
-PARSER.add_argument("-d", "--db", help="DataBase Name", required=True)
-PARSER.add_argument("-r", "--user", help="OpenERP User", required=True)
-PARSER.add_argument("-w", "--passwd", help="OpenERP Password", required=True)
-PARSER.add_argument("-p", "--port",
-                    type=int,
-                    help="Port, 8069 for default", default="8069")
-PARSER.add_argument("-s", "--server",
-                    help="Server IP, 127.0.0.1 for default",
-                    default="127.0.0.1")
+PARSER.add_argument("-u", "--pacuser", help="PAC User", required=True)
+PARSER.add_argument("-p", "--pacpass", help="PAC Password", required=True)
+PARSER.add_argument("-t", "--taxpayer", help="Tax Payer VAT", required=True)
+PARSER.add_argument("-w", "--pacurl", help="PAC url",
+                    default="http://demo-facturacion.finkok.com/servicios/soap/cancel.wsdl")
 PARSER.add_argument("-f", "--filename", help="XML to cancel file path",
                     required=True)
 PARSER.add_argument("-o", "--output", help="Output directory")
-PARSER.add_argument("-c", "--copy", help="Payslip to copy")
 ARGS = PARSER.parse_args()
 
-if ARGS.db is None or ARGS.user is None or ARGS.passwd is None or ARGS.filename is None:
-    print "Must be specified DataBase, User, Password and Directory path"
+if ARGS.pacuser is None or ARGS.pacpass is None or ARGS.taxpayer is None or ARGS.filename is None:
+    print "Must be specified PAC User, PAC Password, Tax Payer and Directory path"
     quit()
 
 if not os.path.isfile(ARGS.filename):
     print "The specified file must exist"
     quit()
 
-DB_NAME = ARGS.db
-USER = ARGS.user
-PASSWD = ARGS.passwd
-SERVER = ARGS.server
-PORT = ARGS.port
-URL = 'http://%s:%s' % (SERVER, PORT)
+PACUSER = ARGS.pacuser
+PACPASS = ARGS.pacpass
+PACURL = ARGS.pacurl
+TAXPAYER = ARGS.taxpayer
 FILE = ARGS.filename
 OUTPUT_DIR = ARGS.output
 
-odoo = odoorpc.ODOO(SERVER, port=PORT)
-odoo.login(DB_NAME, USER, PASSWD)
-
 cert_location = '/home/odoo/odoo-mexico-v2/l10n_mx_facturae_cer/demo/'
-cert_file = 'res_company_facturae_certificate.cer'
-cert_key = 'res_company_facturae_certificate.key'
+cert_file = 'res_company_facturae_certificatepem'
+cert_key = 'res_company_facturae_certificatepemkey'
 uuids = []
-msg = ''
+cancel_results = []
 dict_error = {
+    '000': 'UUID No encontrado',
     '203': 'UUID does not match the RFCsender neither of the applicant',
     '205': 'Not exist UUID',
-    '708': 'Could not connect to the SAT'}
-pac_params_obj = odoo.env['params.pac']
-data_user = odoo.env['res.users'].browse(odoo.env.uid)
-taxpayer_id = data_user.company_id.vat[2::]
-res_pac_id = odoo.env['res.pac'].search(
-    [('company_id', '=', data_user.company_id.id)])
-pac_params_ids = pac_params_obj.search([
-    ('method_type', '=', 'cancelar'),
-    ('res_pac', '=', res_pac_id),
-    ('company_id', '=', data_user.company_id.id),
-    ('active', '=', True), ], limit=1)
-pac_params_id = pac_params_ids and pac_params_ids[0] or False
-if not pac_params_id:
-    print "Check you PAC parameters"
-    quit()
-pac_params_brw = pac_params_obj.browse(pac_params_id)[0]
-username = pac_params_brw.user
-password = pac_params_brw.password
-wsdl_url = pac_params_brw.url_webservice
-__import__('pdb').set_trace()
+    '708': 'Could not connect to the SAT',
+    '201': 'Cancellation process has completly correctly',
+    '202': 'Cancellation process has completly correctly'}
 
 with open(os.path.join(cert_location, cert_file), 'r') as open_file:
     cer_csd = open_file.read()
@@ -85,7 +59,7 @@ with open(os.path.join(cert_location, cert_key), 'r') as open_file:
 key_csd = base64.encodestring(rsa)
 
 try:
-    client = Client(wsdl_url, cache=None)
+    client = Client(PACURL, cache=None)
 except:
     print "Connection lost, verify your internet connection or verify your PAC"
     raise
@@ -93,16 +67,26 @@ except:
 with open(FILE, 'r') as open_file:
     for line in open_file:
         uuids.append(line.strip())
-        uuids_list = client.factory.create("UUIDS")
 
+uuids_list = client.factory.create("UUIDS")
 uuids_list.uuids.string = uuids
-result = client.service.cancel(uuids_list, 'odoo',
-                               'AniaAle1', taxpayer_id,
-                               base64.encodestring(cer_csd),
-                               key_csd)
-if 'Folios' not in result:
-    print "Warning Message: %s" % (result)
+results = client.service.cancel(uuids_list, PACUSER,
+                                PACPASS, TAXPAYER,
+                                base64.encodestring(cer_csd),
+                                key_csd)
+if 'Folios' not in results:
+    print results.CodEstatus
     quit()
-else:
-    if result.Folios[0][0].EstatusUUID in ('201', '202'):
-        print "The process of cancellation has completed correctly"
+
+estatus_uuid = [[d['UUID'], d['EstatusUUID']] for d in results.Folios.Folio]
+for uuid in uuids:
+    if uuid not in [d[0] for d in estatus_uuid]:
+        estatus_uuid.append([uuid, '000'])
+for estatus in estatus_uuid:
+    if estatus[0] not in uuids:
+        cancel_results.append(''.join([estatus[0], ": No encontrado\n"]))
+    if estatus[1] in dict_error:
+        cancel_results.append(''.join(
+                [estatus[0], ": ", dict_error[estatus[1]], "\n"]))
+with open(FILE, 'w') as open_file:
+    open_file.writelines(cancel_results)
