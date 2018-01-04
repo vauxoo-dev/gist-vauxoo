6d56a2ce897a15e6a315d3195c973539b0ff81cc
Moisés López <moylop260@vauxoo.com>
2018-01-04 17:39:04 -0600

    [REF] xsd: Compatible with py3.5 and use file pointers directly

--- xsd/check_xsd.py
+++ xsd/check_xsd.py
@@ -1,16 +1,16 @@
-from io import BytesIO
+from __future__ import print_function
+
 from lxml import etree
 
-schema_str = open('./cfdv33.xsd').read()
-xml_valid = open('./xml.xml').read()
-with BytesIO(schema_str) as xsd:
-    schema_root = etree.parse(xsd)
+schema_str = open('./cfdv33.xsd')
+xml_valid = open('./xml.xml')
+schema_root = etree.parse(schema_str)
 schema = etree.XMLSchema(schema_root)
 try:
-    print '1'
-    tree = etree.fromstring(xml_valid.encode('UTF-8'))
-    print '2'
+    print('1')
+    tree = etree.parse(xml_valid)
+    print('2')
     schema.assertValid(tree)
-    print '3'
+    print('3')
 except etree.DocumentInvalid as ups:
-    print ups
+    print(ups)
