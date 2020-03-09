203fd4d99fe37966d698ff5febd5dacafb744c7d
Luis González <lgonzalez@vauxoo.com>
2020-03-09 23:49:10 +0000

    [FIX] stock_diagnosis: Be more accurate when retrieving qtys from moves
    The following fixes are applied:
    - Take quantities from the stock move lines, instead of the stock moves
    - Consider only moves in the 'Done' state

--- stock_diagnosis/quants_differ_all_stock_moves.sql
+++ stock_diagnosis/quants_differ_all_stock_moves.sql
@@ -5,8 +5,8 @@ reported by their quants is different from the sum of all their stock moves
 
 This is achieved by:
 1) Retrieve product quantities by location according to quants
-2) Retrieve incoming stock moves by location
-3) Retrieve outgoing stock moves by location
+2) Retrieve incoming stock move lines by location
+3) Retrieve outgoing stock move lines by location
 4) Compare results of the previous steps, showing only those when 1) is 
    different from 2) - 3)
 */
@@ -25,11 +25,12 @@ in_move_quantity AS (
     SELECT
         product_id,
         location_dest_id AS location_id,
-        SUM(product_uom_qty)::NUMERIC AS sum_qty
+        SUM(qty_done)::NUMERIC AS sum_qty
     FROM
-        stock_move
+        stock_move_line
     WHERE
         location_id != location_dest_id
+        AND state = 'done'
     GROUP BY
         product_id,
         location_dest_id
@@ -38,11 +39,12 @@ out_move_quantity AS (
     SELECT
         product_id,
         location_id,
-        -SUM(product_uom_qty)::NUMERIC AS sum_qty
+        -SUM(qty_done)::NUMERIC AS sum_qty
     FROM
-        stock_move
+        stock_move_line
     WHERE
         location_id != location_dest_id
+        AND state = 'done'
     GROUP BY
         product_id,
         location_id
