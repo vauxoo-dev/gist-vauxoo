f16fa18ee14b7b80e16499475361b85513c927b9
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2020-07-14 21:27:04 -0500

    [REF] concurrency_script_2.py: Using browse.flush() compatible with >=13.0

--- concurrency_script_2.py
+++ concurrency_script_2.py
@@ -1,29 +1,38 @@
 import logging
 import threading
+import time
 from contextlib import closing
+from random import randint
 
 import odoo
 
 _logger = logging.getLogger(__name__)
+
 WORKERS = 5
-RECORDS = 3
+RECORDS = 2
+MAX_SLEEP = 10
 
 def process():
+    thread_name = threading.current_thread().name
     registry = odoo.modules.registry.Registry.new(self.env.cr.dbname)
     with odoo.api.Environment.manage(), closing(registry.cursor()) as new_cr:
         # Create a new environment with new cursor database
         new_env = odoo.api.Environment(new_cr, self.env.uid, self.env.context)
         # with_env replace original env for this method
         for record in self.env['res.bank'].with_env(new_env).search([], limit=RECORDS):
-            data = record.read(['name'])
-            _logger.info("Running process... (%s)", data)
-            record.write({'name': 'Hello world!'})
-            _logger.info("...process ran (%s)", data)
+            sleep = randint(1, MAX_SLEEP)
+            _logger.info("%s - %s - Sleep %ds - Running process...", thread_name, record, sleep)
+            record.write({'name': 'Hello world %s!' % thread_name})
+            if hasattr(record, 'flush'):  # >=V13.0 to force UPDATE in database
+                record.flush()
+            time.sleep(sleep)
+            _logger.info("%s - %s ...process ran", thread_name, record)
+        new_cr.rollback()
 
 
 threads = []
 for i in range(WORKERS):
-    t = threading.Thread(target=process)
+    t = threading.Thread(target=process, name="Thread %d" % (i+1))
     threads.append(t)
     t.start()
 
