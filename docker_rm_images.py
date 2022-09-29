50c0ef4787e9a64bd93ddf493fdb31db51f7ce30
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2022-09-29 17:00:52 -0500

    [REF] docker_rm_images: Force deleting images and use main method

--- docker_rm_images.py
+++ docker_rm_images.py
@@ -1,15 +1,22 @@
 # pylint: disable=print-used
 import docker
 
-
 cli = docker.from_env()
-images = cli.images.list()
 
 
-for i in images:
-    try:
-        # cli.remove_image(i.id)
-        print("Deleting docker image %s" % i)
-        cli.images.remove(i.id)
-    except docker.errors.APIError as e:
-        print("...image not deleted: %s" % e)
+def rm_images():
+    images = cli.images.list()
+    for i in images:
+        try:
+            print("Deleting docker image %s" % i)
+            cli.images.remove(i.id, force=True)
+        except docker.errors.APIError as e:
+            print("...image not deleted: %s" % e)
+
+
+def main():
+    rm_images()
+
+
+if __name__ == "__main__":
+    main()
