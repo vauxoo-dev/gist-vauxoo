f507e2bf4530181e46d1499379739fa1ff1be7c4
payen000 <payen@vauxoo.com>
2023-02-13 21:46:30 -0700

    [ADD] Added xml_data_generator for v14, v15 and v16

---
+++ xml_data_generator_v14/static/src/views/form_controller.js
@@ -0,0 +1,52 @@
+odoo.define("xml_data_generator.FormController", function (require) {
+    "use strict";
+
+    const FormController = require("web.FormController");
+    const {patch} = require("web.utils");
+    const session = require("web.session");
+    var core = require("web.core");
+    var _t = core._t;
+
+    /**
+     * 'patch' must be used to add the 'Export to XML' action to all form views
+     */
+    patch(FormController, "xml_data_generator.export_to_xml", {
+        /**
+         * Checks if current user has export permissions on current model
+         */
+        willStart() {
+            const sup = this._super(...arguments);
+            const acl = session.user_has_group("base.group_allow_export").then((hasGroup) => {
+                this.isExportEnable = hasGroup;
+            });
+            return Promise.all([sup, acl]);
+        },
+
+        /**
+         * Enables the 'Export to XML' action
+         */
+        _getActionMenuItems() {
+            var menuItems = this._super(...arguments);
+
+            if (!menuItems?.items.other.length || !this.isExportEnable) return menuItems;
+            menuItems.items.other.push({
+                description: _t("Export to XML"),
+                callback: () => {
+                    this.do_action({
+                        type: "ir.actions.act_window",
+                        name: _t("Export to XML"),
+                        res_model: "xml.data.generator",
+                        views: [[false, "form"]],
+                        view_mode: "form",
+                        target: "new",
+                        context: {
+                            default_res_id: this.controlPanelProps.actionMenus.activeIds[0],
+                            default_model_name: this.model.loadParams.modelName,
+                        },
+                    });
+                },
+            });
+            return menuItems;
+        },
+    });
+});
---
+++ xml_data_generator_v16/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import xml_data_generator
