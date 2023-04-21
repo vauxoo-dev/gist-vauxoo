23fe89e55d6b26adbb5b26b3d2fec4b0e974cef0
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2023-04-21 11:32:41 -0600

    [ADD] log_count_sha_key: Count the number of key of the graylog csv file

---
+++ log_count_sha_key.py
@@ -0,0 +1,40 @@
+"""Count the number of occurrences of the "key" of the graylog csv file
+to know where a query is executed too many times
+
+A similar patch is needed in production:
+ - https://github.com/Vauxoo/odoo/pull/547
+"""
+
+import csv
+import re
+from collections import OrderedDict, defaultdict
+
+limit_lines = 1000000
+fname = "graylog-search-result-relative-28800.csv.crdownload"
+with open(fname) as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    counter = defaultdict(int)
+    line_no = 0
+    for row in reader:
+        message = row["message"]
+        match = re.search(r"key: (\w+)", message)
+
+        if not match:
+            continue
+
+        line_no += 1
+
+        if line_no >= limit_lines:
+            break
+
+        key = match.groups()[0]
+        counter[key] += 1
+
+
+counter_ordered = OrderedDict(sorted(counter.items(), key=lambda item: item[1], reverse=True))
+limit = 10
+for line_no, (key, key_count) in enumerate(counter_ordered.items()):
+    if line_no > limit:
+        break
+    print(f"count {key_count}, sha {key}")
