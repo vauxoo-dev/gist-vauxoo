d11749642fbff1f6765ada619f0913196972c880
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2023-07-19 21:52:21 -0600

    [REF] requirements_clean: Small fixes and lints

--- requirements_clean.py
+++ requirements_clean.py
@@ -1,19 +1,32 @@
-import click
-
+# pylint:disable=print-used
 import re
+
+import click
 
 
 @click.command()
-@click.option('--project-requirements-path', help='Project requirements.txt')
-@click.option('--odoo-requirements-path', help='Odoo requirements.txt file path', default="/home/odoo/instance/odoo/requirements.txt", show_default=True)
-@click.option('--full-requirements-path', help='DeployV full_requirements.txt file path', default="/home/odoo/full_requirements.txt", show_default=True)
+@click.option("--project-requirements-path", help="Project requirements.txt")
+@click.option(
+    "--odoo-requirements-path",
+    help="Odoo requirements.txt file path",
+    default="/home/odoo/instance/odoo/requirements.txt",
+    show_default=True,
+)
+@click.option(
+    "--full-requirements-path",
+    help="DeployV full_requirements.txt file path",
+    default="/home/odoo/full_requirements.txt",
+    show_default=True,
+)
 def clean_requirements(**kwargs):
     odoo_requirements_path = kwargs.get("odoo_requirements_path")
     project_requirements_path = kwargs.get("project_requirements_path")
     full_requirements_path = kwargs.get("full_requirements_path")
 
-    with open(odoo_requirements_path) as f_odoo_requirements, open(project_requirements_path) as f_project_requirements, open(full_requirements_path) as f_full_requirements:
-        
+    with open(odoo_requirements_path) as f_odoo_requirements, open(
+        project_requirements_path
+    ) as f_project_requirements, open(full_requirements_path) as f_full_requirements:
+
         full_requirements_packages = get_packages(f_full_requirements)
 
         project_requirements_packages = get_packages(f_project_requirements)
@@ -21,11 +34,11 @@ def clean_requirements(**kwargs):
         odoo_requirements_packages = get_packages(f_odoo_requirements)
 
         missing_exclude = full_requirements_packages - odoo_requirements_packages - project_requirements_packages
-        print("missing_exclude", missing_exclude)
+        print("Missing packages from requirements_exclude.txt %s" % "\n".join(sorted(missing_exclude)))
 
 
 def get_packages(lines):
-    split_re = re.compile(r'[<>=#]')
+    split_re = re.compile(r"[<>=#]")
     packages = set()
     for line in lines:
         package = split_re.split(line)[0].strip()
@@ -35,5 +48,5 @@ def get_packages(lines):
     return packages
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     clean_requirements()
