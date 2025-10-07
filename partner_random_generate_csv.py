616c95c188355ced4e21609665e386da784ae792
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2025-10-07 18:16:04 +0000

    [REF] partner_random_generate_csv: Auto-detects not-null columns and define default values

--- partner_random_generate_csv.py
+++ partner_random_generate_csv.py
@@ -5,11 +5,34 @@ from faker import Faker
 
 fake = Faker()
 
+fields = ["name", "email", "ref", "create_uid", "create_date", "write_uid", "write_date"]
+fields_required_possible = {
+    # if you database has installed a few extra modules it could raise not null errors
+    # Better define a manual default
+    "autopost_bills": "ask",
+    "group_rfq": "default",
+    "group_on": "default",
+}
 
-def generate_csv(file_name, total_records):
+def generate_csv(file_name, total_records, cursor):
+    cursor.execute("""
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name = 'res_partner'
+          AND table_schema = 'public'
+          AND is_nullable = 'NO'
+          AND column_default IS NULL
+    """)
+    columns_not_null = [i[0] for i in cursor.fetchall()]
+    missing_columns = set(columns_not_null) - set(fields)
+    missing_columns_wo_default = missing_columns - set(fields_required_possible)
+    if missing_columns_wo_default:
+        raise UserWarning("There are missing columns %s" % missing_columns)
+    missing_columns_w_default = missing_columns & set(fields_required_possible)
+    fields.extend(missing_columns_w_default)
     with open(file_name, mode="w") as file:
         writer = csv.writer(file)
-        writer.writerow(["name", "email", "ref", "create_uid", "create_date", "write_uid", "write_date"])
+        writer.writerow(fields)
 
         create_uid = 1
         write_uid = 1
@@ -18,29 +41,28 @@ def generate_csv(file_name, total_records):
             name = fake.name()
             email = fake.email()
             ref = fake.random_number(digits=10, fix_len=True)
-            writer.writerow([name, email, ref, create_uid, "NOW()", write_uid, "NOW()"])
+            default_values = [fields_required_possible[col] for col in missing_columns_w_default]
+            writer.writerow([name, email, ref, create_uid, "NOW()", write_uid, "NOW()"] + default_values)
 
 
-def copy_from_csv(file_name):
+def copy_from_csv(file_name, cursor):
+    with open(file_name) as file:
+        cursor.copy_expert(
+            "COPY res_partner(%s) FROM STDIN WITH CSV HEADER" % ",".join(fields),
+            file,
+        )
+        conn.commit()
+
+
+
+if __name__ == "__main__":
     # Use PG* environment variables to connect to database
     conn = psycopg2.connect()
     cursor = conn.cursor()
     try:
-        with open(file_name) as file:
-            cursor.copy_expert(
-                """
-                COPY res_partner(name, email, ref, create_uid, create_date, write_uid, write_date)
-                FROM STDIN WITH CSV HEADER
-                """,
-                file,
-            )
-            conn.commit()
+        fname = "partners.csv"
+        generate_csv(fname, total_records=2000000, cursor=cursor)
+        copy_from_csv(fname, cursor)
     finally:
         cursor.close()
         conn.close()
-
-
-if __name__ == "__main__":
-    fname = "partners.csv"
-    # generate_csv(fname, total_records=2000000)
-    copy_from_csv(fname)
